d0594606ebbeb9b9c51a4717823593f182ff2fd5
midgardhero <63206815+midgardhero@users.noreply.github.com>
2022-02-14 21:19:29 -0300

    Add URL absolute to return entry-list
    in the current version, there is no absolute URL setting that points a return to the main journal page, when there is confirmation of the addition of a new entry, which can be solved by adding the get_absolute_URL function with a reverse for the entry_list.

--- django-diary/source_code_final/entries/models.py
+++ django-diary/source_code_final/entries/models.py
@@ -9,6 +9,9 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.title
-
+    
+    def get_absolute_url(self):
+        return reverse('entry-list')
+    
     class Meta:
         verbose_name_plural = "Entries"
